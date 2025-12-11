e6cd8c02b597f180fbd5f00db1c553caba171937
vayaafyvas-lang <vayaafyvas@gmail.com>
2025-12-11 23:19:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,124 @@
-let items = [
-	"Сделать проектную работу",
-	"Полить цветы",
-	"Пройти туториал по Реакту",
-	"Сделать фронт для своего проекта",
-	"Прогуляться по улице в солнечный день",
-	"Помыть посуду",
+// Список начальных задач
+const items = [
+  "Сделать проектную работу",
+  "Полить цветы",
+  "Пройти туториал по Реакту",
+  "Сделать фронт для своего проекта",
+  "Прогуляться по улице в солнечный день",
+  "Помыть посуду",
 ];
 
+
+// Элементы страницы
 const listElement = document.querySelector(".to-do__list");
 const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
+
+// Загрузка списка задач
 function loadTasks() {
 
-}
-
-function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    return JSON.parse(storedTasks);
+  }
+  return items;
 
 }
 
+
+// Сохранение задач в локальное хранилище
+function saveTasks(taskList) {
+
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+
+}
+
+
+// Получение задач из DOM
 function getTasksFromDOM() {
 
+  const taskElements = document.querySelectorAll('.to-do__item-text');
+  const tasksArray = [];
+  taskElements.forEach(element => {
+    tasksArray.push(element.innerHTML);
+  });
+  return tasksArray;
+
 }
 
-function saveTasks(tasks) {
+
+// Создание элемента задачи
+function createItem(taskContent) {
+
+  const itemTemplate = document.getElementById("to-do__item-template");
+  const itemClone = itemTemplate.content.querySelector(".to-do__item").cloneNode(true);
+  const taskTextElement = itemClone.querySelector(".to-do__item-text");
+  const deleteBtn = itemClone.querySelector(".to-do__item-button_type_delete");
+  const duplicateBtn = itemClone.querySelector(".to-do__item-button_type_duplicate");
+  const editBtn = itemClone.querySelector(".to-do__item-button_type_edit");
+  taskTextElement.innerHTML = taskContent;
+
+
+  // Обработчик удаления задачи
+  deleteBtn.addEventListener('click', () => {
+    itemClone.remove();
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+  });
+
+
+  // Обработчик копирования задачи
+  duplicateBtn.addEventListener('click', () => {
+    const duplicatedContent = taskTextElement.innerHTML;
+    const duplicatedItem = createItem(duplicatedContent);
+    listElement.prepend(duplicatedItem);
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+  });
+
+
+  // Обработчик редактирования задачи
+  editBtn.addEventListener('click', () => {
+    taskTextElement.setAttribute('contenteditable', 'true');
+    taskTextElement.focus();
+  });
+
+
+  // Обработчик сохранения после редактирования
+  taskTextElement.addEventListener('blur', () => {
+    taskTextElement.setAttribute('contenteditable', 'false');
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+  });
+
+
+  return itemClone;
 
 }
 
+
+// Обработчик отправки формы
+formElement.addEventListener('submit', (event) => {
+
+  event.preventDefault();
+  const newTaskText = inputElement.value.trim();
+  if (newTaskText) {
+    const newTaskElement = createItem(newTaskText);
+    listElement.prepend(newTaskElement);
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+    inputElement.value = '';
+  }
+
+});
+
+
+// Загрузка и отображение задач при загрузке страницы
+const initialTasks = loadTasks();
+initialTasks.forEach(task => {
+
+  const taskElement = createItem(task);
+  listElement.append(taskElement);
+
+});
